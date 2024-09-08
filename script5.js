2c180746eaa4cf17f331e725627ed7a90ff10a51
rakesh <rakeshn2204@gmail.con>
2024-09-08 23:20:32 +0530

    js Objects

---
+++ script5.js
@@ -0,0 +1,31 @@
+
+const person = {
+    name: "john",
+    age: 30,
+    city: "New York",
+};
+
+//disply all objects using for loop
+
+let text = "";
+for (let x in person) {
+    text += person[x] + " "
+};
+
+document.getElementById('demo').innerHTML = text;
+
+const myArray = Object.values(person);
+
+document.getElementById("demo1").innerHTML = myArray;
+
+const fruits = {
+    Bananas: 300,
+    Oranges: 200,
+    Apple: 500
+};
+
+let txt = "";
+for (let [fruit, amount] of Object.entries(fruits)) {
+    text += "<br>" + fruit + " : " + amount;
+}
+document.getElementById('demo2').innerHTML = text;
